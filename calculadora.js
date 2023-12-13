8ce16a60ed93601e335e7a1088a84ae3232cf09b
CamilaVerso <camilaverso2@gmail.com>
2023-12-12 21:45:07 -0300

    add: função dividir

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,23 @@
+const input = require('readline-sync');
+
 class Calculadora {
     constructor() {
         
     }
+
+    dividir() {
+
+        const numero1 = parseFloat(input.question('Digite o primeiro numero: '));
+        const numero2 = parseFloat(input.question('Digite o segundo numero: '));
+
+
+        if (isNaN(numero1) || isNaN(numero2)) {
+            console.log('Por favor, insira números válidos.');
+            return;
+        }
+
+        const resultado = numero1 / numero2;
+        console.log(`Resultado da divisão: ${resultado}`);
+    }
 }
+
